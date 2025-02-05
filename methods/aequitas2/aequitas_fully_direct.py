5e1edabfd7c127a3a507c5a596831a14cb231ec2
Vaunorage <vaunorage@gmail.com>
2025-02-05 10:15:08 -0500

    fix aequitas

--- methods/aequitas2/aequitas_fully_direct.py
+++ methods/aequitas2/aequitas_fully_direct.py
@@ -1,6 +1,4 @@
-import os, sys
 from methods.aequitas2.utils import get_adult_dataset, prepare_data_for_fairness_testing
-from path import HERE
 from methods.aequitas2 import utils as ut
 import numpy as np
 from methods.aequitas2 import loss_funcs as lf
@@ -8,6 +6,11 @@ import random
 import time
 from scipy.optimize import basinhopping
 from methods.aequitas2 import config
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 
 random.seed(time.time())
 start_time = time.time()
@@ -51,10 +54,71 @@ param_probability_change_size = 0.001
 loss_function = lf._logistic_loss
 sep_constraint = 0
 
-gamma = None
 
-model = ut.train_model(X, Y, sensitive, loss_function, 1, 0, sep_constraint, sensitive_attrs,
-                       sensitive_attrs_to_cov_thresh, gamma)
+class ModelFactory:
+    def __init__(self):
+        self.models = {
+            'logistic': self._create_logistic_regression,
+            'svm': self._create_svm,
+            'random_forest': self._create_random_forest,
+            'neural_network': self._create_neural_network
+        }
+
+    def _create_logistic_regression(self):
+        return LogisticRegression(random_state=42, max_iter=1000)
+
+    def _create_svm(self):
+        return SVC(kernel='rbf', probability=True, random_state=42)
+
+    def _create_random_forest(self):
+        return RandomForestClassifier(n_estimators=100, random_state=42)
+
+    def _create_neural_network(self):
+        return MLPClassifier(hidden_layer_sizes=(100, 50), random_state=42)
+
+    def get_model(self, model_type='logistic'):
+        model_creator = self.models.get(model_type.lower())
+        if model_creator is None:
+            raise ValueError(f"Unsupported model type: {model_type}")
+        return model_creator()
+
+
+def train_model_sklearn(X, y, model_type='logistic'):
+    """
+    Train a sklearn model of the specified type
+    """
+    factory = ModelFactory()
+    model = factory.get_model(model_type)
+    model.fit(X, y)
+    return model
+
+
+def evaluate_input_sklearn(inp, model, sensitive_param, threshold):
+    """
+    Evaluate input using sklearn models
+    """
+    inp0 = [int(i) for i in inp]
+    inp1 = [int(i) for i in inp]
+
+    inp0[sensitive_param - 1] = 0
+    inp1[sensitive_param - 1] = 1
+
+    # Reshape for sklearn prediction
+    inp0 = np.array(inp0).reshape(1, -1)
+    inp1 = np.array(inp1).reshape(1, -1)
+
+    # Get probabilities instead of just predictions
+    out0 = model.predict_proba(inp0)[:, 1]
+    out1 = model.predict_proba(inp1)[:, 1]
+
+    return float(abs(out0 - out1) > threshold)
+
+
+# gamma = None
+# model = ut.train_model(X, Y, sensitive, loss_function, 1, 0, sep_constraint, sensitive_attrs,
+#                        sensitive_attrs_to_cov_thresh, gamma)
+
+model = train_model_sklearn(X, Y, 'logistic')
 
 
 def normalise_probability():
@@ -66,13 +130,16 @@ def normalise_probability():
         param_probability[i] = float(param_probability[i]) / float(probability_sum)
 
 
-class Local_Perturbation(object):
+# First, define the base perturbation class
+class BasePerturbation(object):
     def __init__(self, stepsize=1):
         self.stepsize = stepsize
 
+
+# Fully Directed implementation
+class FullyDirectedLocalPerturbation(BasePerturbation):
     def __call__(self, x):
         s = self.stepsize
-        # Updated xrange to range
         param_choice = np.random.choice(range(params), p=param_probability)
         perturbation_options = [-1, 1]
 
@@ -84,7 +151,6 @@ class Local_Perturbation(object):
             direction_choice = np.random.choice(perturbation_options)
 
         x[param_choice] = x[param_choice] + (direction_choice * perturbation_unit)
-
         x[param_choice] = max(input_bounds[param_choice][0], x[param_choice])
         x[param_choice] = min(input_bounds[param_choice][1], x[param_choice])
 
@@ -93,7 +159,6 @@ class Local_Perturbation(object):
         if (ei and direction_choice == -1) or (not ei and direction_choice == 1):
             direction_probability[param_choice] = min(direction_probability[param_choice] +
                                                       (direction_probability_change_size * perturbation_unit), 1)
-
         elif (not ei and direction_choice == -1) or (ei and direction_choice == 1):
             direction_probability[param_choice] = max(direction_probability[param_choice] -
                                                       (direction_probability_change_size * perturbation_unit), 0)
@@ -106,6 +171,59 @@ class Local_Perturbation(object):
             normalise_probability()
 
         return x
+
+
+# Semi-directed implementation
+class SemiDirectedLocalPerturbation(BasePerturbation):
+    def __call__(self, x):
+        s = self.stepsize
+        param_choice = random.randint(0, params - 1)
+        perturbation_options = [-1, 1]
+
+        direction_choice = np.random.choice(perturbation_options,
+                                            p=[direction_probability[param_choice],
+                                               (1 - direction_probability[param_choice])])
+
+        if (x[param_choice] == input_bounds[param_choice][0]) or (x[param_choice] == input_bounds[param_choice][1]):
+            direction_choice = np.random.choice(perturbation_options)
+
+        x[param_choice] = x[param_choice] + (direction_choice * perturbation_unit)
+        x[param_choice] = max(input_bounds[param_choice][0], x[param_choice])
+        x[param_choice] = min(input_bounds[param_choice][1], x[param_choice])
+
+        ei = evaluate_input(x)
+
+        if (ei and direction_choice == -1) or (not ei and direction_choice == 1):
+            direction_probability[param_choice] = min(direction_probability[param_choice] +
+                                                      (direction_probability_change_size * perturbation_unit), 1)
+        elif (not ei and direction_choice == -1) or (ei and direction_choice == 1):
+            direction_probability[param_choice] = max(direction_probability[param_choice] -
+                                                      (direction_probability_change_size * perturbation_unit), 0)
+
+        return x
+
+
+# Random implementation
+class RandomLocalPerturbation(BasePerturbation):
+    def __call__(self, x):
+        s = self.stepsize
+        param_choice = random.randint(0, params - 1)
+        direction_choice = random.choice([-1, 1])
+
+        x[param_choice] = x[param_choice] + (direction_choice * perturbation_unit)
+        x[param_choice] = max(input_bounds[param_choice][0], x[param_choice])
+        x[param_choice] = min(input_bounds[param_choice][1], x[param_choice])
+
+        return x
+
+
+def get_perturbation_strategy(strategy='fully-directed'):
+    strategies = {
+        'fully-directed': FullyDirectedLocalPerturbation,
+        'semi-directed': SemiDirectedLocalPerturbation,
+        'random': RandomLocalPerturbation
+    }
+    return strategies.get(strategy, FullyDirectedLocalPerturbation)()
 
 
 class Global_Discovery(object):
@@ -124,14 +242,7 @@ class Global_Discovery(object):
 
 
 def evaluate_input(inp):
-    inp0 = [int(i) for i in inp]
-    inp1 = [int(i) for i in inp]
-
-    inp0[sensitive_param - 1] = 0
-    inp1[sensitive_param - 1] = 1
-    out0 = np.sign(np.dot(model, inp0))
-    out1 = np.sign(np.dot(model, inp1))
-    return float(abs(out0 - out1) > threshold)
+    return evaluate_input_sklearn(inp, model, sensitive_param, threshold)
 
 
 def evaluate_global(inp):
@@ -140,15 +251,21 @@ def evaluate_global(inp):
 
     inp0[sensitive_param - 1] = 0
     inp1[sensitive_param - 1] = 1
-    out0 = np.sign(np.dot(model, inp0))
-    out1 = np.sign(np.dot(model, inp1))
+
+    # Reshape for sklearn prediction
+    inp0_reshaped = np.array(inp0).reshape(1, -1)
+    inp1_reshaped = np.array(inp1).reshape(1, -1)
+
+    # Get probabilities instead of just predictions
+    out0 = model.predict_proba(inp0_reshaped)[:, 1]
+    out1 = model.predict_proba(inp1_reshaped)[:, 1]
+
     tot_inputs.add(tuple(inp0))
 
     if (abs(out0 - out1) > threshold and tuple(inp0) not in global_disc_inputs):
         global_disc_inputs.add(tuple(inp0))
         global_disc_inputs_list.append(inp0)
 
-    # Return float instead of boolean
     return float(not abs(out0 - out1) > threshold)
 
 
@@ -158,8 +275,15 @@ def evaluate_local(inp):
 
     inp0[sensitive_param - 1] = 0
     inp1[sensitive_param - 1] = 1
-    out0 = np.sign(np.dot(model, inp0))
-    out1 = np.sign(np.dot(model, inp1))
+
+    # Reshape for sklearn prediction
+    inp0_reshaped = np.array(inp0).reshape(1, -1)
+    inp1_reshaped = np.array(inp1).reshape(1, -1)
+
+    # Get probabilities instead of just predictions
+    out0 = model.predict_proba(inp0_reshaped)[:, 1]
+    out1 = model.predict_proba(inp1_reshaped)[:, 1]
+
     tot_inputs.add(tuple(inp0))
 
     if (abs(out0 - out1) > threshold and (tuple(inp0) not in global_disc_inputs)
@@ -167,14 +291,11 @@ def evaluate_local(inp):
         local_disc_inputs.add(tuple(inp0))
         local_disc_inputs_list.append(inp0)
 
-    # Return float instead of boolean
     return float(not abs(out0 - out1) > threshold)
 
-
 minimizer = {"method": "L-BFGS-B"}
-
 global_discovery = Global_Discovery()
-local_perturbation = Local_Perturbation()
+local_perturbation = get_perturbation_strategy(strategy='fully-directed')
 
 basinhopping(evaluate_global, initial_input, stepsize=1.0, take_step=global_discovery,
              minimizer_kwargs=minimizer, niter=global_iteration_limit)
