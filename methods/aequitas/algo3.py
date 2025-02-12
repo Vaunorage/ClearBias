1ba20f45c4fa540439ff150490b4bf0d32406980
Vaunorage <vaunorage@gmail.com>
2025-02-11 22:34:01 -0500

    fix aequitas

--- methods/aequitas/algo3.py
+++ methods/aequitas/algo3.py
@@ -9,165 +9,7 @@ from data_generator.main import get_real_data, DiscriminationData
 from methods.utils import train_sklearn_model
 
 
-class Local_Perturbation(object):
-    """
-    The  implementation of local perturbation
-    """
-
-    def __init__(self, preds, conf: DiscriminationData, sensitive_param, param_probability,
-                 param_probability_change_size,
-                 direction_probability, direction_probability_change_size, step_size):
-        """
-        Initial function of local perturbation
-        :param sess: TF session
-        :param preds: the model's symbolic output
-        :param x: input placeholder
-        :param conf: the configuration of dataset
-        :param sensitive_param: the index of sensitive feature
-        :param param_probability: the probabilities of features
-        :param param_probability_change_size: the step size for changing probability
-        :param direction_probability: the probabilities of perturbation direction
-        :param direction_probability_change_size:
-        :param step_size: the step size of perturbation
-        """
-        self.preds = preds
-        self.conf = conf
-        self.sensitive_param = sensitive_param
-        self.param_probability = param_probability
-        self.param_probability_change_size = param_probability_change_size
-        self.direction_probability = direction_probability
-        self.direction_probability_change_size = direction_probability_change_size
-        self.step_size = step_size
-        self.perturbation_unit = 1
-
-    def __call__(self, x):
-        """
-        Local perturbation
-        :param x: input instance for local perturbation
-        :return: new potential individual discriminatory instance
-        """
-        # randomly choose the feature for perturbation
-        param_choice = np.random.choice(range(len(self.conf.attr_columns)), p=self.param_probability)
-
-        # randomly choose the direction for perturbation
-        perturbation_options = [-1, 1]
-        direction_choice = np.random.choice(perturbation_options, p=[self.direction_probability[param_choice],
-                                                                     (1 - self.direction_probability[param_choice])])
-        if (x[param_choice] == self.conf.input_bounds[param_choice][0]) or (
-                x[param_choice] == self.conf.input_bounds[param_choice][1]):
-            direction_choice = np.random.choice(perturbation_options)
-
-        # perturbation
-        x[param_choice] = x[param_choice] + (direction_choice * self.step_size)
-
-        # clip the generating instance with each feature to make sure it is valid
-        x[param_choice] = max(self.conf.input_bounds[param_choice][0], x[param_choice])
-        x[param_choice] = min(self.conf.input_bounds[param_choice][1], x[param_choice])
-
-        # check whether the test case is an individual discriminatory instance
-        ei = check_for_error_condition(self.conf, self.preds, x)
-
-        # update the probabilities of directions
-        if (ei != int(x[self.sensitive_param - 1]) and direction_choice == -1) or (
-                not (ei != int(x[self.sensitive_param - 1])) and direction_choice == 1):
-            self.direction_probability[param_choice] = min(self.direction_probability[param_choice] +
-                                                           (
-                                                                   self.direction_probability_change_size * self.perturbation_unit),
-                                                           1)
-        elif (not (ei != int(x[self.sensitive_param - 1])) and direction_choice == -1) or (
-                ei != int(x[self.sensitive_param - 1]) and direction_choice == 1):
-            self.direction_probability[param_choice] = max(self.direction_probability[param_choice] -
-                                                           (
-                                                                   self.direction_probability_change_size * self.perturbation_unit),
-                                                           0)
-
-        # update the probabilities of features
-        if ei != int(x[self.sensitive_param - 1]):
-            self.param_probability[param_choice] = self.param_probability[
-                                                       param_choice] + self.param_probability_change_size
-            self.normalise_probability()
-        else:
-            self.param_probability[param_choice] = max(
-                self.param_probability[param_choice] - self.param_probability_change_size, 0)
-            self.normalise_probability()
-
-        return x
-
-    def normalise_probability(self):
-        """
-        Normalize the probability
-        :return: probability
-        """
-        probability_sum = 0.0
-        for prob in self.param_probability:
-            probability_sum = probability_sum + prob
-
-        for i in range(self.conf.params):
-            self.param_probability[i] = float(self.param_probability[i]) / float(probability_sum)
-
-
-class Global_Discovery(object):
-    """
-    The  implementation of global perturbation
-    """
-
-    def __init__(self, conf: DiscriminationData):
-        """
-        Initial function of global perturbation
-        :param conf: the configuration of dataset
-        """
-        self.conf = conf
-
-    def __call__(self, x):
-        """
-        Global perturbation
-        :param x: input instance for local perturbation
-        :return: new potential individual discriminatory instance
-        """
-        # clip the generating instance with each feature to make sure it is valid
-        for i in range(len(self.conf.attr_columns)):
-            x[i] = random.randint(self.conf.input_bounds[i][0], self.conf.input_bounds[i][1])
-        return x
-
-
-def check_for_error_condition(conf: DiscriminationData, preds, t):
-    inp_df = pd.DataFrame([t], columns=conf.attr_columns)
-    original_pred = preds(inp_df)[0]
-
-    # Get all unique values for each protected attribute
-    protected_values = {}
-    for attr in conf.protected_attributes:
-        protected_values[attr] = sorted(conf.dataframe[attr].unique())
-
-    # Generate all possible combinations of protected attributes
-    attr_names = list(protected_values.keys())
-    attr_values = list(protected_values.values())
-    combinations = list(product(*attr_values))
-
-    test_cases = []
-    for combination in combinations:
-        # Skip if it's identical to the input case
-        if all(inp_df[attr].iloc[0] == value
-               for attr, value in zip(attr_names, combination)):
-            continue
-
-        new_case = inp_df.copy()
-        for attr, value in zip(attr_names, combination):
-            new_case[attr] = value
-        test_cases.append(new_case)
-
-    if not test_cases:
-        return 0
-
-    test_cases_df = pd.concat(test_cases)
-    test_cases_df['outcome'] = preds(test_cases_df.to_numpy())
-
-    max_discrimination = (test_cases_df['outcome'] - original_pred).max()
-
-    return max_discrimination
-
-
-def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local, step_size, iter):
+def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local, step_size):
     """
     The implementation of AEQUITAS_Fully_Connected
     :param dataset: the name of testing dataset
@@ -178,6 +20,172 @@ def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local
     :param step_size: the step size of perturbation
     :return:
     """
+
+    all_discrimination = set()
+
+    class Local_Perturbation(object):
+        """
+        The  implementation of local perturbation
+        """
+
+        def __init__(self, preds, conf: DiscriminationData, sensitive_param, param_probability,
+                     param_probability_change_size,
+                     direction_probability, direction_probability_change_size, step_size):
+            """
+            Initial function of local perturbation
+            :param sess: TF session
+            :param preds: the model's symbolic output
+            :param x: input placeholder
+            :param conf: the configuration of dataset
+            :param sensitive_param: the index of sensitive feature
+            :param param_probability: the probabilities of features
+            :param param_probability_change_size: the step size for changing probability
+            :param direction_probability: the probabilities of perturbation direction
+            :param direction_probability_change_size:
+            :param step_size: the step size of perturbation
+            """
+            self.preds = preds
+            self.conf = conf
+            self.sensitive_param = sensitive_param
+            self.param_probability = param_probability
+            self.param_probability_change_size = param_probability_change_size
+            self.direction_probability = direction_probability
+            self.direction_probability_change_size = direction_probability_change_size
+            self.step_size = step_size
+            self.perturbation_unit = 1
+
+        def __call__(self, x):
+            """
+            Local perturbation
+            :param x: input instance for local perturbation
+            :return: new potential individual discriminatory instance
+            """
+            # randomly choose the feature for perturbation
+            param_choice = np.random.choice(range(len(self.conf.attr_columns)), p=self.param_probability)
+
+            # randomly choose the direction for perturbation
+            perturbation_options = [-1, 1]
+            direction_choice = np.random.choice(perturbation_options, p=[self.direction_probability[param_choice],
+                                                                         (1 - self.direction_probability[
+                                                                             param_choice])])
+            if (x[param_choice] == self.conf.input_bounds[param_choice][0]) or (
+                    x[param_choice] == self.conf.input_bounds[param_choice][1]):
+                direction_choice = np.random.choice(perturbation_options)
+
+            # perturbation
+            x[param_choice] = x[param_choice] + (direction_choice * self.step_size)
+
+            # clip the generating instance with each feature to make sure it is valid
+            x[param_choice] = max(self.conf.input_bounds[param_choice][0], x[param_choice])
+            x[param_choice] = min(self.conf.input_bounds[param_choice][1], x[param_choice])
+
+            # check whether the test case is an individual discriminatory instance
+            ei, max_row = check_for_error_condition(self.conf, self.preds, x)
+
+            # update the probabilities of directions
+            if (ei != 0 and direction_choice == -1) or (not ei != 0 and direction_choice == 1):
+                self.direction_probability[param_choice] = min(self.direction_probability[param_choice] + (
+                        self.direction_probability_change_size * self.perturbation_unit), 1)
+
+            elif (not ei != 0 and direction_choice == -1) or (ei != 0 and direction_choice == 1):
+                self.direction_probability[param_choice] = max(self.direction_probability[param_choice] - (
+                        self.direction_probability_change_size * self.perturbation_unit), 0)
+
+            # update the probabilities of features
+            if ei != 0:
+                self.param_probability[param_choice] = self.param_probability[
+                                                           param_choice] + self.param_probability_change_size
+                self.normalise_probability()
+            else:
+                self.param_probability[param_choice] = max(
+                    self.param_probability[param_choice] - self.param_probability_change_size, 0)
+                self.normalise_probability()
+
+            return x
+
+        def normalise_probability(self):
+            """
+            Normalize the probability
+            :return: probability
+            """
+            probability_sum = 0.0
+            for prob in self.param_probability:
+                probability_sum = probability_sum + prob
+
+            for i in range(len(self.conf.attr_columns)):
+                self.param_probability[i] = float(self.param_probability[i]) / float(probability_sum)
+
+    class Global_Discovery(object):
+        """
+        The  implementation of global perturbation
+        """
+
+        def __init__(self, conf: DiscriminationData):
+            """
+            Initial function of global perturbation
+            :param conf: the configuration of dataset
+            """
+            self.conf = conf
+
+        def __call__(self, x):
+            """
+            Global perturbation
+            :param x: input instance for local perturbation
+            :return: new potential individual discriminatory instance
+            """
+            # clip the generating instance with each feature to make sure it is valid
+            for i in range(len(self.conf.attr_columns)):
+                x[i] = random.randint(self.conf.input_bounds[i][0], self.conf.input_bounds[i][1])
+            return x
+
+    def check_for_error_condition(conf: DiscriminationData, preds, t):
+        inp_df = pd.DataFrame([t], columns=conf.attr_columns)
+        original_pred = preds(inp_df)[0]
+
+        # Get all unique values for each protected attribute
+        protected_values = {}
+        for attr in conf.protected_attributes:
+            protected_values[attr] = sorted(conf.dataframe[attr].unique())
+
+        # Generate all possible combinations of protected attributes
+        attr_names = list(protected_values.keys())
+        attr_values = list(protected_values.values())
+        combinations = list(product(*attr_values))
+
+        test_cases = []
+        for combination in combinations:
+            # Skip if it's identical to the input case
+            if all(inp_df[attr].iloc[0] == value
+                   for attr, value in zip(attr_names, combination)):
+                continue
+
+            new_case = inp_df.copy()
+            for attr, value in zip(attr_names, combination):
+                new_case[attr] = value
+            test_cases.append(new_case)
+
+        if not test_cases:
+            return 0, None
+
+        test_cases_df = pd.concat(test_cases)
+        test_predictions = preds(test_cases_df)
+        test_cases_df['outcome'] = test_predictions
+
+        discriminations = test_cases_df['outcome'] - original_pred
+        max_discrimination = discriminations.max()
+        max_discrimination_idx = discriminations.idxmax()
+        max_discrimination_row = test_cases_df.loc[max_discrimination_idx]
+
+        discriminations_df = test_cases_df[discriminations > 0]
+
+        inp_df['outcome'] = original_pred
+
+        if discriminations_df.shape[0] != 0:
+            for el in discriminations_df.to_numpy():
+                all_discrimination.add((tuple(map(int, inp_df.to_numpy()[0])), tuple(map(int, el))))
+
+        return max_discrimination, max_discrimination_row
+
     start = time.time()
 
     # Train the model using the new function
@@ -213,7 +221,7 @@ def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local
     minimizer = {"method": "L-BFGS-B"}
 
     def evaluate_local(inp):
-        result = check_for_error_condition(dataset, preds, inp)
+        result, max_row = check_for_error_condition(dataset, preds, inp)
         inp_key = pd.DataFrame([inp.astype('int').tolist()], columns=dataset.attr_columns)
         inp_key = list(inp_key.drop(columns=dataset.protected_attributes).to_numpy())[0]
         tot_inputs.add(tuple(inp_key))
@@ -252,7 +260,7 @@ def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local
         inp_key = list(inp_key.drop(columns=dataset.protected_attributes).to_numpy())[0]
         tot_inputs.add(tuple(inp_key))
 
-        result = check_for_error_condition(dataset, preds, inp)
+        result, max_row = check_for_error_condition(dataset, preds, inp)
 
         # if get an individual discriminatory instance
         if result > 0 and (tuple(inp_key) not in global_disc_inputs) and (tuple(inp_key) not in local_disc_inputs):
@@ -262,27 +270,81 @@ def aequitas(dataset: DiscriminationData, sensitive_param, max_global, max_local
             basinhopping(evaluate_local, inp, stepsize=1.0, take_step=local_perturbation,
                          minimizer_kwargs=minimizer, niter=max_local)
 
+    res_df = []
+    case_id = 0
+    for org, counter_org in all_discrimination:
+        indv1 = pd.DataFrame(org, columns=dataset.attr_columns)
+        indv2 = pd.DataFrame(counter_org, columns=dataset.attr_columns)
+
+        indv_key1 = "|".join(str(x) for x in indv1[dataset.attr_columns].iloc[0])
+        indv_key2 = "|".join(str(x) for x in indv2[dataset.attr_columns].iloc[0])
+
+        # Add the additional columns
+        indv1['indv_key'] = indv_key1
+        indv2['indv_key'] = indv_key2
+
+        # Create couple_key as before
+
+        couple_key = f"{indv_key1}-{indv_key2}"
+        diff_outcome = abs(indv1['outcome'] - indv2['outcome'])
+
+        df_res = pd.concat([indv1, indv2])
+        df_res['couple_key'] = couple_key
+        df_res['diff_outcome'] = diff_outcome
+        df_res['case_id'] = case_id
+        res_df.append(df_res)
+        case_id += 1
+
+    if len(res_df) != 0:
+        res_df = pd.concat(res_df)
+    else:
+        res_df = pd.DataFrame([])
+
     # print the overview information of result
     print("Total Inputs are " + str(len(tot_inputs)))
     print("Total discriminatory inputs of global search- " + str(len(global_disc_inputs)))
     print("Total discriminatory inputs of local search- " + str(len(local_disc_inputs)))
     print("Percentage discriminatory inputs - " + str(float(len(global_disc_inputs_list) + len(local_disc_inputs_list))
                                                       / float(len(tot_inputs)) * 100))
+    end_time = time.time()
+    execution_time = end_time - start
+    return res_df, execution_time, tot_inputs
 
 
 def main():
     ge, ge_schema = get_real_data('adult')
     start = time.time()
-    ITER = 30
+    all_inputs = []
+    all_exec_times = []
+    results_df = []
+    ITER = 1
     for i in range(ITER):
-        aequitas(dataset=ge,
-                 sensitive_param=ge.sensitive_indices,
-                 max_global=1000,
-                 max_local=10000,
-                 step_size=1,
-                 iter=i)
+        res_df, execution_time, total_inputs = aequitas(dataset=ge,
+                                                        sensitive_param=ge.sensitive_indices,
+                                                        max_global=100,
+                                                        max_local=1000,
+                                                        step_size=1)
         end = time.time()
+        all_inputs.extend(total_inputs)
+        all_exec_times.append(execution_time)
+        results_df.append(res_df)
+
         print('total time:' + str(end - start))
+
+    results_df = pd.concat(results_df)
+    results_df.drop_duplicates(subset='couple_key', keep='first', inplace=True)
+
+    tsn = len(all_inputs)
+    dsn = results_df.shape[0] / len(all_inputs)
+
+    metrics = {
+        "TSN": tsn,
+        "DSN": dsn,
+        "DSS": round(sum(all_exec_times) / len(all_exec_times), 2),
+        "SUR": round(dsn / tsn, 2)
+    }
+
+    return results_df, metrics
 
 
 if __name__ == '__main__':
