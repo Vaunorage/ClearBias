0765d33c83abbf71580bc67286fa3eed774ab40a
vaunorage <vaunorage@gmail.com>
2025-05-27 00:15:32 -0400

    fix unvertainty quantification

--- experiments/synthetic_datasets_experiments/analysis.py
+++ experiments/synthetic_datasets_experiments/analysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cbb30556475c78e527daec217702df3289ff4fedb1a8a7fff59da4021332ca2
-size 28291
+oid sha256:e3acf7c5d5de3a427bfae5e692360aae84ff13a3bfa90e43f8fd002718bea3c5
+size 29591
