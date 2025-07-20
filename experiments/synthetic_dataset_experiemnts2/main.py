8c5d6b713bcbb17c6ffca8e13dfa56935918120d
Vaunorage <vaunorage@gmail.com>
2025-07-20 14:33:57 -0400

    make datasets more diverse

--- experiments/synthetic_dataset_experiemnts2/main.py
+++ experiments/synthetic_dataset_experiemnts2/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e09f080004c12509e7cdb8ec7f717ca38d44cb732000b4ec640dcadeeaba97c2
-size 6789
+oid sha256:0a607258a521a95adba9805f3a13cc3995fb27eedc16a79a6b122eefc401f17c
+size 7426
