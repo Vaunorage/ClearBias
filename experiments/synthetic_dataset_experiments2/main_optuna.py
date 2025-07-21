5a3a07f035c8ec821ce3d5f73eb99da5cb4a251f
PhilippeRacicot <vaunorage@gmail.com>
2025-07-21 20:49:59 +0000

    added running experiments optuna

--- experiments/synthetic_dataset_experiments2/main_optuna.py
+++ experiments/synthetic_dataset_experiments2/main_optuna.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:371972b05e6284e9bd57bfa192e8bec7dcb9ff11ae063533ffaa4de15e4cec6f
-size 8856
+oid sha256:7ca865121af17f0498244862e29c929f1222a92d05e8b082ae07e0b5672da034
+size 9009
