b7265f6d236b8d8740cd5951c0aebc7d32772818
Vaunorage <vaunorage@gmail.com>
2025-07-23 17:33:19 -0400

    fixed optuna main

--- experiments/synthetic_dataset_experiments2/main_optuna.py
+++ experiments/synthetic_dataset_experiments2/main_optuna.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:670c677db57297dedd0988ed608fea2074b7e641d8e1910d17a08a8f0a136b93
-size 16202
+oid sha256:6e1f8e95cab30c4baa96a8cc663ad564b9b6362a434e1441775c4b7e1b806bd1
+size 16205
