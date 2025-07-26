fa6d56c6e7a4d7216fd971ff25b828f1ab865949
PhilippeRacicot <vaunorage@gmail.com>
2025-07-26 01:42:17 +0000

    fixes to fliptest

--- experiments/synthetic_dataset_experiments2/main_optuna.py
+++ experiments/synthetic_dataset_experiments2/main_optuna.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b57c6f65b505b5daaf638c5dfa27c1d08c1a7eeddeb93a5115b8b9d22d2d179
-size 17056
+oid sha256:5a79f0e5f4b96e5b4f231918a68073ce18a6025c9000d323de51540662162c51
+size 17053
