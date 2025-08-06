c9204e00ebadd428f329704da9b94105ad249e7b
vaunorage <vaunorage@gmail.com>
2025-08-06 14:53:58 -0400

    feat: implement optimized pattern matching and detailed effectiveness analysis

--- experiments/synthetic_dataset_experiments2/analyze_efficiency.py
+++ experiments/synthetic_dataset_experiments2/analyze_efficiency.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d10f31886d03b4abdd9b745806b52ce8ca817ba5fb3abe114c2493444cbd445
-size 24470
+oid sha256:fac466b3f718a5c66c648ad80c40d71602047018dd33daa1cd8f89659e696c43
+size 24919
