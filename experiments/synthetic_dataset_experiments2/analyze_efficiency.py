b14a50bed1c1bd4bcf702e9174db3fddaa128add
vaunorage <vaunorage@gmail.com>
2025-08-06 20:25:46 -0400

    feat: implement optimized pattern matching and detailed result tracking for bias analysis

--- experiments/synthetic_dataset_experiments2/analyze_efficiency.py
+++ experiments/synthetic_dataset_experiments2/analyze_efficiency.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd268784b0a26ea3d316f1df3fc83006f5dccaf253bb64405bc0b822390a54fa
-size 25968
+oid sha256:a204278316b0a6c07f6a35a63c4b9b3e7fba2e96fabb1f8c46c6b9bc0c430d81
+size 25879
