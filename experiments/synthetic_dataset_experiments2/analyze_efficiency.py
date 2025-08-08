f9aa6c758581d94e52cfdba280c7bb76a174308c
vaunorage <vaunorage@gmail.com>
2025-08-08 14:44:24 -0400

    refactor: reimplement efficiency analysis with optimized pattern matching and incremental database storage

--- experiments/synthetic_dataset_experiments2/analyze_efficiency.py
+++ experiments/synthetic_dataset_experiments2/analyze_efficiency.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a204278316b0a6c07f6a35a63c4b9b3e7fba2e96fabb1f8c46c6b9bc0c430d81
-size 25879
+oid sha256:c48199bd9ed1d8e0532430ce25183e89b6084515c5c79964a9371aad4e515a0f
+size 25949
