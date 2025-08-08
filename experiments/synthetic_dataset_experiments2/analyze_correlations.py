4fc4e519bb3f609e233320ae8de7aa10adbd77e4
vaunorage <vaunorage@gmail.com>
2025-08-08 12:12:40 -0400

    feat: implement correlation and CATE analysis for synthetic dataset experiments

--- experiments/synthetic_dataset_experiments2/analyze_correlations.py
+++ experiments/synthetic_dataset_experiments2/analyze_correlations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6b0a42c18518ddaed5481626c6a7958e7ab19688abf375dae8dc091dcea58cc
-size 35211
+oid sha256:9cb42cdc9444f8a64013d32f80d91e90e4ab93f346b8274c0d8d4d543aee3565
+size 35642
