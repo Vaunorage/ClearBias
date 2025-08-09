ec6d9156aa1acc3b75f389feb93b97add2826a44
vaunorage <vaunorage@gmail.com>
2025-08-09 13:56:13 -0400

    feat: reimplement correlation analysis with group-level means and treatment effects

--- experiments/synthetic_dataset_experiments2/analyze_correlations.py
+++ experiments/synthetic_dataset_experiments2/analyze_correlations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2177670b7ae14c6c3a100fe9b54fee3df59036682d063169bb63e520b34aa9b0
-size 58909
+oid sha256:69d2c5b11da71ec0fef7697e292e10bd68688ee35f66b7d514a806aca2973b29
+size 58943
