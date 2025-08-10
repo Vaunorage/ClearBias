3196c4779bc7d03529bbdb623c41cef0f7452da6
vaunorage <vaunorage@gmail.com>
2025-08-10 18:09:24 -0400

    feat: implement CATE analysis for calculated features and algorithm parameters

--- experiments/synthetic_dataset_experiments2/analyze_correlations.py
+++ experiments/synthetic_dataset_experiments2/analyze_correlations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2636512f8b3602c5abe76041bfdffaacf78c59c269f388340e24b9a5fe9fc14f
-size 73758
+oid sha256:7e3249765ffb13df6469109a0a6f8e07d060b202342174421ee1f1c5dd1c9cd3
+size 70960
--- experiments/synthetic_dataset_experiments2/db_utils.py
+++ experiments/synthetic_dataset_experiments2/db_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a12c5ea566df618327d5d2eae51613a8adba912f16d2609f30d84ef6debc5cb6
-size 1577
+oid sha256:d1192e8cec158ea141870900bf5ea7f973da25075e0bea7d9cc30ccc57b10d06
+size 1704
