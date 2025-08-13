00e5b3264c4cbdc21c474c313f96be1a342b2923
vaunorage <vaunorage@gmail.com>
2025-08-13 13:06:51 -0400

    feat: implement correlation and treatment effect analysis for synthetic dataset experiments

--- experiments/synthetic_dataset_experiments2/analyze_correlations.py
+++ experiments/synthetic_dataset_experiments2/analyze_correlations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:367e25b719184919d27ee286c17781899423ea01223e07de90a9badb9422f665
-size 88675
+oid sha256:be5515ce912f54314eb7c119261a7de49bf02d1b2b524fedf43032a4d9b20db5
+size 101261
